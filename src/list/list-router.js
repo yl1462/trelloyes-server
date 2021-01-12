ef1cd98d344c764ee7d7f12ca168042c02c61457
Yuri Liang <68250067+yl1462@users.noreply.github.com>
2021-01-11 22:35:18 -0600

    first commit

---
+++ src/list/list-router.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const uuid = require('uuid/v4');
+const logger = require('../logger');
+const { cards, lists } = require('../store');
+
+const listRouter = express.Router();
+const bodyParser = express.json();
+
+listRouter
+  .route('/list')
+  .get((req, res) => {
+    res.json(lists);
+  })
+  .post(bodyParser, (req, res) => {
+    const { header, cardIds = [] } = req.body;
+
+    if (!header) {
+      logger.error('header is required');
+      return res.status(400).send('Invalida data');
+    }
+
+    if (!cardIds.length > 0) {
+      let valid = true;
+
+      cardIds.forEach(cid => {
+        const card = cards.find(c => c.id == cid);
+        if (!card) {
+          logger.error(`Card with id ${cid} not found in cards array.`);
+          valid = false;
+        }
+      });
+      if (!valid) {
+        return res.status(400).send('Invalid data');
+      }
+    }
+    const id = uuid();
+    const list = {
+      id,
+      header,
+      cardIds
+    };
+    lists.push(list);
+    logger.info(`List with id ${id} created`);
+    res
+      .status(201)
+      .send(`http://localhost:8000/list/${id}`)
+      .json({ id });
+  });
+
+listRouter
+  .route('/list/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    const list = lists.find(li => li.id == id);
+
+    if (!list) {
+      logger.error(`List with id ${id} not found.`);
+    }
+    res.json(list);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    const listIndex = lists.findIndex(li => li.id == id);
+
+    if (listIndex === -1) {
+      logger.error(`List with id ${id} not found.`);
+      return res.status(400).send('Not found');
+    }
+
+    lists.splice(listIndex, 1);
+
+    logger.info(`List with id ${id} deleted`);
+    res.status(204).end();
+  });
+
+module.exports = listRouter;
